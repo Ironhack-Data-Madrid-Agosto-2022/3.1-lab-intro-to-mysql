08b80850dc5cbe484cc5186c01f855c92c57f75c
Davidmcii <davidmartincasero@hotmail.com>
2022-08-22 19:04:01 +0200

    ejercicio completado

---
+++ your-code/seeding.sql
@@ -0,0 +1,50 @@
+INSERT INTO cars (VIN, marca, model, color) VALUES('DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 'Gray')
+
+INSERT INTO cars (VIN, marca, model, color) VALUES('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 'Blue')
+
+INSERT INTO cars (VIN, marca, model, color) VALUES('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 'Red')
+
+INSERT INTO cars (VIN, marca, model, color) VALUES('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 'White')
+
+INSERT INTO cars (VIN, marca, model, color) VALUES('HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 'Silver')
+
+INSERT INTO cars (VIN, marca, model, color) VALUES('DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 'Grey')
+
+INSERT INTO cars (VIN, marca, model, color) VALUES('DAM41UDN3CHU2WVF7', 'Volvo', 'V60', 'Grey')
+
+INSERT INTO (cID, name, phone, email, address, city, zip) VALUES('10001', 'Pablo Picasso', '+34 636 17 63 82', '', 'Paseo de la Chopera, 14' ,'Madrid', '28045')
+
+INSERT INTO customer(cID, name, phone, email, address, city, zip) VALUES('10001', 'Pablo Picasso', '+34 636 17 63 82', '', 'Paseo de la Chopera, 14' ,'Madrid', '28045')
+
+INSERT INTO customer(cID, name, phone, email, address, city, zip) VALUES('10001', 'Pablo Picasso', '+34 636 17 63 82', '', 'Paseo de la Chopera, 14' ,'Madrid', '28045')
+
+INSERT INTO customer(cID, name, phone, email, address, city, zip) VALUES('10001', 'Pablo Picasso', '+34 636 17 63 82', '', 'Paseo Chopera, 14' ,'Madrid', '28045')
+
+INSERT INTO customer(cID, name, phone, email, address, city, zip) VALUES('20001', 'Hedy Lamarr', '+43 1 514442250', '', 'Weiglgasse 10',' Viena',  '1150')
+
+INSERT INTO customer(cID, name, phone, email, address, city, zip) VALUES('30001', 'Katherine Johnson', '+1 202 358 0000', '', '300 E St SW', 'Washington', '20546')
+
+INSERT INTO salesperson(sID, name, store) VALUES('00001','Petey Cruiser','Madrid')
+
+INSERT INTO salesperson(sID, name, store) VALUES('00002','Anna Sthesia','Barcelona')
+
+INSERT INTO salesperson(sID, name, store) VALUES('00003','Paul Molive','Berlin')
+
+INSERT INTO salesperson(sID, name, store) VALUES ('00004','Gail Forcewind','Paris')
+
+INSERT INTO salesperson(sID, name, store) VALUES ('00005','Paige Turner','Mimia')
+
+INSERT INTO salesperson(sID, name, store) VALUES ('00006','Bob Frapples','Mexico City')
+
+INSERT INTO salesperson(sID, name, store) VALUES ('00007','Walter Melon','Amsterdam')
+
+INSERT INTO salesperson(sID, name, store) VALUES ('00007','Walter Melon','Amsterdam')
+
+INSERT INTO salesperson(sID, name, store) VALUES ('00008','Shonda Leer','São Paulo')
+
+INSERT INTO invoices(iID, name, date,salesperson_sID, customer_cID, cars_VIN) VALUES('0','852399038','2018-08-22','00001','10001','HKNDGS7CU31E9Z7JW')
+
+INSERT INTO invoices(iID, name, date,salesperson_sID, customer_cID, cars_VIN) VALUES('1','731166526','2018-12-31','00004','20001','3K096I98581DHSNUP')
+
+INSERT INTO invoices(iID, name, date,salesperson_sID, customer_cID, cars_VIN) VALUES('2','271135104','2019-01-22','00003','30001','DAM41UDN3CHU2WVF6')
+
---
+++ your-code/update.sql
@@ -0,0 +1,17 @@
+UPDATE salesperson
+SET store = 'Miami'
+WHERE sID='00005';
+
+UPDATE customer
+SET email = 'ppicasso@gmail.com'
+WHERE cID='10001';
+
+UPDATE customer
+SET email = 'lamarr@hollywood.com'
+WHERE cID='20001';
+
+UPDATE customer
+SET email = 'hello@nasa.gov'
+WHERE cID='30001';
+
+
