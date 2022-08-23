c55dda0128f0ad2660a819be087e2994b983c3e0
Mar sds <mar26sds@gmail.com>
2022-08-23 17:15:16 +0200

    mar

---
+++ your-code/delete.sql
@@ -0,0 +1,4 @@
+select* 
+from Cars;
+delete from Cars
+where idcars= 4;
---
+++ your-code/update.sql
@@ -0,0 +1,24 @@
+update Salesperson
+set Store='Miami'
+where idsales=4;
+
+select*
+from Salesperson;
+
+select* 
+from customers;
+
+update customers
+set Email='ppicasso@gmail.com'
+where idcustomers=0;
+
+update customers
+set Email='lamarr_hd@hollywood.com'
+where idcustomers=1;
+
+update customers
+set Email='hello@nasa.gov'
+where idcustomers=2;
+
+select* 
+from customers;
